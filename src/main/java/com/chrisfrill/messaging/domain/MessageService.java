96ba65d5d91f4c1c60c44eef160d3b865e7f53f0
Krisztian Fodor <fkrisztian93@gmail.com>
2021-08-31 16:56:52 +0200

    Create MessageService to interact with repository

---
+++ src/main/java/com/chrisfrill/messaging/domain/MessageService.java
@@ -0,0 +1,25 @@
+package com.chrisfrill.messaging.domain;
+
+import com.chrisfrill.messaging.domain.model.MessageEntity;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Log4j2
+@Service
+public class MessageService {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Mono<MessageEntity> save(MessageEntity messageEntity) {
+        log.info("Saving new message to Redis");
+        return Mono.just(messageRepository.save(messageEntity));
+    }
+
+    public Flux<MessageEntity> findAll() {
+        log.info("Getting messages from Redis");
+        return Flux.fromIterable(messageRepository.findAll());
+    }
+}
