421d359c23df9e4ed37da6e6a68cfd51fae0197d
Krisztian Fodor <fkrisztian93@gmail.com>
2021-08-31 18:28:47 +0200

    Add CORS configuration

---
+++ src/main/java/com/chrisfrill/messaging/configuration/http/CorsGlobalConfiguration.java
@@ -0,0 +1,19 @@
+package com.chrisfrill.messaging.configuration.http;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET")
+                .allowedMethods("POST")
+                .maxAge(3600);
+    }
+}
